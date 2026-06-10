caf060674eef575f13b3b6f782a4600f99f2c91e
Beforerr <zzj956959688@gmail.com>
2026-06-10 00:15:46 -0700

    ci: add AirspeedVelocity benchmark workflow
    - benchmark/benchmarks.jl: BenchmarkTools SUITE from test/perf_test.jl (elx + mms groups)
    - mms 50MB file downloaded from SPDF, cached in tempdir
    - Benchmark.yml: PR comment via pull_request_target, concurrency cancel

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,39 @@
+using BenchmarkTools
+using CommonDataFormat
+using Downloads
+
+const SUITE = BenchmarkGroup()
+
+# elb file is committed to the repo, so it ships with the package at any rev
+const ELX_FILE = joinpath(pkgdir(CommonDataFormat), "data", "elb_l2_epdef_20210914_v01.cdf")
+
+# mms file (50 MB) is gitignored; download once and cache across revisions
+function download_data(url, filename = basename(url))
+    dir = joinpath(tempdir(), "CommonDataFormat_benchmark_data")
+    mkpath(dir)
+    path = joinpath(dir, filename)
+    isfile(path) || Downloads.download(url, path)
+    return path
+end
+
+const MMS_FILE = download_data("https://spdf.gsfc.nasa.gov/pub/data/mms/mms1/scm/srvy/l2/scsrvy/2019/03/mms1_scm_srvy_l2_scsrvy_20190301_v2.2.0.cdf")
+
+full_load(fname) = collect(CDFDataset(fname))
+
+let ds = CDFDataset(ELX_FILE), var = ds["elb_pef_hs_Epat_eflux"]
+    g = SUITE["elx"] = BenchmarkGroup()
+    g["var_access"] = @benchmarkable $ds["elb_pef_hs_Epat_eflux"]
+    g["sum_array"] = @benchmarkable sum(Array($var))
+    g["sum_lazy"] = @benchmarkable sum($var)
+    g["sum_var_access"] = @benchmarkable sum($ds["elb_pef_hs_Epat_eflux"])
+    g["full_load"] = @benchmarkable full_load(ELX_FILE)
+end
+
+let ds = CDFDataset(MMS_FILE), var = ds["mms1_scm_acb_gse_scsrvy_srvy_l2"]
+    g = SUITE["mms"] = BenchmarkGroup()
+    g["var_access"] = @benchmarkable $ds["mms1_scm_acb_gse_scsrvy_srvy_l2"]
+    g["sum_array"] = @benchmarkable sum(Array($var))
+    g["sum_slice"] = @benchmarkable sum($var[:, 100:100000])
+    g["sum_var_access"] = @benchmarkable sum($var[:, 100:100000])
+    g["full_load"] = @benchmarkable full_load(MMS_FILE)
+end
